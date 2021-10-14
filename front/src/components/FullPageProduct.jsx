660681e331ea0a03ae265e54c39d427294ac4f33
Ricardo Rosas <ichy_wero@hotmail.com>
2021-10-14 09:09:56 -0500

    Prueba1 Ruteo

--- front/src/components/FullPageProduct.jsx
+++ front/src/components/FullPageProduct.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const LOCAL_BACK = "http://localhost:5000";
 
-const ProductItem = ({data,addToCart,link}) => {
-const {_id,name,image,description,brand,category,price,countInStock} = data;
+const FullPageProduct = ({data,addToCart}) => {
+ const {_id,name,image,description,brand,category,price,countInStock} = data;
   return (
     <div className="productItem">
       <div className="image-container">
@@ -16,13 +16,13 @@ const {_id,name,image,description,brand,category,price,countInStock} = data;
         <p><span>Category:</span>{" "+category}</p>
         <p><span>Price:</span>{" $"+price}</p>
         <p><span>In Stock:</span>{" "+countInStock}</p>
-        <div className="buttons">
-        {link}
         <button className="addToCartButton" onClick={() => addToCart(_id)}> <span className="textButtonAdd"> Add to Cart </span> </button>
-        </div>
-      </div>  
-    </div>
-  );
-};
+      </div>
 
-export default ProductItem;
+      <h2>AQUI VA TODO LO DEL PRODUCTO ALV</h2>
+      
+    </div>
+  )
+}
+
+export default FullPageProduct
